1cf8329b806f8e19d7aa3a3f6940de1434ef02ac
fabius <fabioclf@gmail.com>
2025-05-12 09:02:54 -0400

    feat(mod5): adiciona tarefa Hello World com PrimeiraClasse

---
+++ mod5/PrimeiroProjeto/src/PrimeiraClasse.java
@@ -0,0 +1,9 @@
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+        System.out.print("Hello World");
+        } else {
+            System.out.print("Hello " + args[0]);
+        }
+    }
+}
